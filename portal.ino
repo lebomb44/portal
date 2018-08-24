3d0df8a91218c526772fec4933933ffc34ebd852
LeBomb <cambon.olivier@gmail.com>
2018-08-24 08:17:46 +0200

    Add second limiter

--- portal.ino
+++ portal.ino
@@ -1,5 +1,6 @@
 #include <Fifo_U16.h>
 #include <HomeEasy.h>
+//#include <HT12E.h>
 #define F_CPU 16000000UL
 #include <util/delay.h>
 
@@ -7,20 +8,23 @@
 #define RELAY_LEFT_pin 7
 #define RELAY_RIGHT_pin 8
 #define IR_POWER_pin 10
-#define LIMITER_pin 3
+/* LIMITER used at start of CLOSE and end of OPEN */
+#define LIMITER_LEFT_pin 5
+/* LIMITER used at start of OPEN and end of CLOSE */
+#define LIMITER_RIGHT_pin 3
 #define RF_IN_pin 2
 #define MOTOR_SENSE_pin A0
 #define MOTOR_PWM_pin 11
 
-#define PORTAL_FULL_SLOT (PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT + PORTAL_SLOW_SLOT)
-#define PORTAL_SLOW_SLOT 4000
-#define PORTAL_CRUISE_SLOT 5000
+#define PORTAL_FULL_SLOT ((PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT) * 4)
+#define PORTAL_SLOW_SLOT 2000
+#define PORTAL_CRUISE_SLOT 6000
 
 #define PORTAL_CMD_CLOSE 0
 #define PORTAL_CMD_OPEN  1
 
-#define MOTOR_MAX_CURRENT 110
-#define MOTOR_MAX_SLOW_CURRENT 100
+#define MOTOR_MAX_CURRENT 190
+#define MOTOR_MAX_SLOW_CURRENT 190
 
 HomeEasy homeEasy;
 int portal_last_cmd = PORTAL_CMD_CLOSE;
@@ -35,6 +39,7 @@ uint16_t motor_max_current_nb = 0;
 uint16_t get_force(uint16_t _begin_position, uint16_t _position) {
   uint16_t _begin_force = 0;
   uint16_t _end_force = 0;
+  uint16_t _force = 0;
 
   if(PORTAL_SLOW_SLOT > (_position - _begin_position)) {
     _begin_force = _position - _begin_position;
@@ -53,15 +58,20 @@ uint16_t get_force(uint16_t _begin_position, uint16_t _position) {
     if((PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT) <= _position) {
       _end_force = PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT - _position;
       if(PORTAL_SLOW_SLOT/2 > _end_force) {
-        _end_force = PORTAL_SLOW_SLOT/2;
+        _end_force = PORTAL_SLOW_SLOT/4;
       }
     }
   }
 
   /* Set to MAX force to unstruck the portal */
-  if(50 > _position) { return PORTAL_SLOW_SLOT; }
+  if(50 > _position) { _force = PORTAL_SLOW_SLOT; }
   /* Else normal work */
-  else { return min(_begin_force, _end_force); }
+  else { _force = min(_begin_force, _end_force); }
+
+  if((50 == _position - _begin_position) || (PORTAL_SLOW_SLOT == _position - _begin_position) || (PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT == _position) || (PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT - 1 == _position)) {
+    Serial.print("FORCE at "); Serial.print(_position, DEC); Serial.print(" (begin at "); Serial.print(_begin_position, DEC); Serial.print(") = "); Serial.println(_force, DEC);
+  }
+  return _force;
 }
 
 void setup()
@@ -76,7 +86,8 @@ void setup()
   digitalWrite(RELAY_RIGHT_pin, LOW);
   pinMode(IR_POWER_pin, OUTPUT);
   digitalWrite(IR_POWER_pin, LOW);
-  pinMode(LIMITER_pin, INPUT);
+  pinMode(LIMITER_LEFT_pin, INPUT);
+  pinMode(LIMITER_RIGHT_pin, INPUT);
   pinMode(RF_IN_pin, INPUT);
   pinMode(MOTOR_SENSE_pin, INPUT);
   pinMode(MOTOR_PWM_pin, OUTPUT);
@@ -104,40 +115,78 @@ void loop()
     || ((0xFCC302 == homeEasy.rxGetManufacturer()) && (0x0 == homeEasy.rxGetGroup()) && (0x2 == homeEasy.rxGetDevice()))) {
       digitalWrite(IR_POWER_pin, HIGH);
       if(PORTAL_CMD_CLOSE == homeEasy.rxGetStatus()) {
-        digitalWrite(RELAY_LEFT_pin, HIGH);
-        digitalWrite(RELAY_RIGHT_pin, LOW);
-        portal_last_cmd = portal_cmd;
-        portal_cmd = PORTAL_CMD_CLOSE;
-        if((PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT) < portal_position) {
-          portal_position = PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT;
+        if(LOW == digitalRead(LIMITER_RIGHT_pin)) {
+          digitalWrite(RELAY_LEFT_pin, HIGH);
+          digitalWrite(RELAY_RIGHT_pin, LOW);
+          portal_last_cmd = portal_cmd;
+          portal_cmd = PORTAL_CMD_CLOSE;
+          if((PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT) < portal_position) {
+            portal_position = PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT;
+          }
+          if(PORTAL_CMD_CLOSE == portal_last_cmd) {
+            portal_start_position = portal_position;
+          }
+          if(PORTAL_CMD_OPEN == portal_last_cmd) {
+            portal_start_position = PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT - portal_position;
+            portal_position = portal_start_position;
+          }
+          if(HIGH == digitalRead(LIMITER_LEFT_pin)) {
+            portal_start_position = 0;
+            portal_position = portal_start_position;
+            Serial.println("CLOSE from LIMITER");
+          }
+          portal_cmd_accepted = true;
+          Serial.print("CLOSE at: "); Serial.println(portal_position, DEC);
         }
-        if(PORTAL_CMD_CLOSE == portal_last_cmd) {
-          portal_start_position = portal_position;
-        }
-        if(PORTAL_CMD_OPEN == portal_last_cmd) {
-          portal_start_position = PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT - portal_position;
+        else {
+          /* Use this new command to reset to known position */
+          digitalWrite(MOTOR_PWM_pin, LOW);
+          digitalWrite(RELAY_LEFT_pin, LOW);
+          digitalWrite(RELAY_RIGHT_pin, LOW);
+          digitalWrite(IR_POWER_pin, LOW);
+          portal_start_position = PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT;
           portal_position = portal_start_position;
+          portal_cmd_accepted = false;
+          portal_force = 0;
+          Serial.println("Already CLOSE");
         }
-        portal_cmd_accepted = true;
-        Serial.print("CLOSE at: "); Serial.println(portal_position, DEC);
       }
       if(PORTAL_CMD_OPEN == homeEasy.rxGetStatus()) {
-        digitalWrite(RELAY_LEFT_pin, LOW);
-        digitalWrite(RELAY_RIGHT_pin, HIGH);
-        portal_last_cmd = portal_cmd;
-        portal_cmd = PORTAL_CMD_OPEN;
-        if((PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT) < portal_position) {
-          portal_position = PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT;
+        if(LOW == digitalRead(LIMITER_LEFT_pin)) {
+          digitalWrite(RELAY_LEFT_pin, LOW);
+          digitalWrite(RELAY_RIGHT_pin, HIGH);
+          portal_last_cmd = portal_cmd;
+          portal_cmd = PORTAL_CMD_OPEN;
+          if((PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT) < portal_position) {
+            portal_position = PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT;
+          }
+          if(PORTAL_CMD_OPEN == portal_last_cmd) {
+            portal_start_position = portal_position;
+          }
+          if(PORTAL_CMD_CLOSE == portal_last_cmd) {
+            portal_start_position = PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT - portal_position;
+            portal_position = portal_start_position;
+          }
+          if(HIGH == digitalRead(LIMITER_RIGHT_pin)) {
+            portal_start_position = 0;
+            portal_position = portal_start_position;
+            Serial.println("OPEN from LIMITER");
+          }
+          portal_cmd_accepted = true;
+          Serial.print("OPEN at: "); Serial.println(portal_position, DEC);
         }
-        if(PORTAL_CMD_OPEN == portal_last_cmd) {
-          portal_start_position = portal_position;
-        }
-        if(PORTAL_CMD_CLOSE == portal_last_cmd) {
-          portal_start_position = PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT - portal_position;
+        else {
+          /* Use this new command to reset to known position */
+          digitalWrite(MOTOR_PWM_pin, LOW);
+          digitalWrite(RELAY_LEFT_pin, LOW);
+          digitalWrite(RELAY_RIGHT_pin, LOW);
+          digitalWrite(IR_POWER_pin, LOW);
+          portal_start_position = PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT;
           portal_position = portal_start_position;
+          portal_cmd_accepted = false;
+          portal_force = 0;
+          Serial.println("Already OPEN");
         }
-        portal_cmd_accepted = true;
-        Serial.print("OPEN at: "); Serial.println(portal_position, DEC);
       }
     }
     homeEasy.rxRelease();
@@ -146,11 +195,11 @@ void loop()
   if(true == portal_cmd_accepted) {
     _delay_ms(1);
     portal_position++;
-    if(PORTAL_FULL_SLOT < portal_position) {
-      portal_position = PORTAL_FULL_SLOT;
-    }
+
+    /* Compute force related to the position */
     portal_force = get_force(portal_start_position, portal_position);
 
+    /* Compute maximum current authorized related to the position */
     if((PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT) > portal_position) {
       if(100 > portal_position) {
         motor_max_current = 1024;
@@ -164,7 +213,7 @@ void loop()
     }
   }
 
-  /* Back to idle mode : everything OFF */
+  /* Check over current filtered on several acquisitions */
   int motor_currentRead = analogRead(MOTOR_SENSE_pin);
   if(motor_max_current < motor_currentRead) {
     motor_max_current_nb++;
@@ -174,30 +223,56 @@ void loop()
   }
 
   /* Check over current */
-  if(100 < motor_max_current_nb) {
+  if(1000 < motor_max_current_nb) {
+    motor_max_current_nb = 0;
     digitalWrite(MOTOR_PWM_pin, LOW);
     digitalWrite(RELAY_LEFT_pin, LOW);
     digitalWrite(RELAY_RIGHT_pin, LOW);
     digitalWrite(IR_POWER_pin, LOW);
+    Serial.print("CURRENT MAX detected: "); Serial.println(motor_currentRead, DEC);
     portal_cmd_accepted = false;
     portal_force = 0;
-    Serial.print("CURRENT MAX detected: "); Serial.println(motor_currentRead, DEC);
   }
-  /* Check Limiter */
-  if(2000 < portal_position) {
-    if(HIGH == digitalRead(LIMITER_pin)) {
+  /* Check Limiter at end of CLOSE */
+  if(PORTAL_CMD_CLOSE == portal_cmd) {
+    if(HIGH == digitalRead(LIMITER_RIGHT_pin)) {
+      /* As the limiter triggered we are at the end of the rail */
+      portal_position = PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT;
       digitalWrite(MOTOR_PWM_pin, LOW);
       digitalWrite(RELAY_LEFT_pin, LOW);
       digitalWrite(RELAY_RIGHT_pin, LOW);
       digitalWrite(IR_POWER_pin, LOW);
+      if(true == portal_cmd_accepted) { Serial.println("LIMITER detected at end of CLOSE"); }
       portal_cmd_accepted = false;
       portal_force = 0;
-      /* As the limiter triggered we are at the end of the rail */
-      portal_position = PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT;
-      Serial.println("LIMITER detected");
     }
   }
-  
+  /* Check Limiter at end of OPEN */
+  if(PORTAL_CMD_OPEN == portal_cmd) {
+    if(HIGH == digitalRead(LIMITER_LEFT_pin)) {
+      /* As the limiter triggered we are at the end of the rail */
+      portal_position = PORTAL_SLOW_SLOT + PORTAL_CRUISE_SLOT + PORTAL_SLOW_SLOT;
+      digitalWrite(MOTOR_PWM_pin, LOW);
+      digitalWrite(RELAY_LEFT_pin, LOW);
+      digitalWrite(RELAY_RIGHT_pin, LOW);
+      digitalWrite(IR_POWER_pin, LOW);
+      if(true == portal_cmd_accepted) { Serial.println("LIMITER detected at end of OPEN"); }
+      portal_cmd_accepted = false;
+      portal_force = 0;
+    }
+  }
+  /* Timeout */
+  if(PORTAL_FULL_SLOT < portal_position) {
+    portal_position = PORTAL_FULL_SLOT;
+    digitalWrite(MOTOR_PWM_pin, LOW);
+    digitalWrite(RELAY_LEFT_pin, LOW);
+    digitalWrite(RELAY_RIGHT_pin, LOW);
+    digitalWrite(IR_POWER_pin, LOW);
+    if(true == portal_cmd_accepted) { Serial.print("TIMEOUT detected"); }
+    portal_cmd_accepted = false;
+    portal_force = 0;
+  }
+
   /* Clone LED status on RF signal */
   //digitalWrite(MOTOR_PWM_pin, HIGH);
   analogWrite(MOTOR_PWM_pin, map(portal_force, 0, PORTAL_SLOW_SLOT, 0, 255));
